34fafa374ec8915a92ee952d094c7ab2ef0aa958
Simo00m <rmxali2003@gmail.com>
2024-06-18 14:52:35 -0700

    Ali Khudhair: Creating code structure. Will work on pseudocode.

---
+++ Connect4Game/Connect4Game/Program.cs
@@ -0,0 +1,16 @@
+﻿// Connect Four Game Project
+
+// 4 Core Classes aside from Main Class Program
+// controller class
+// information about the game (game board)
+// 2 classes that extend Player abstract class
+
+// Make the game playable for 2 human players
+// optional for AI (computer)
+class Program
+{
+    static void Main(string[] args)
+    {
+            
+    }
+}
